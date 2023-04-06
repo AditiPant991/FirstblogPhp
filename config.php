ee9c3407b5a111e9e8e14a1530bcef3bb0b57fe0
AditiPant <aditipant991@gmail.com>
2023-04-06 10:49:21 +0530

    firstblogxammp1

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+$config = mysqli_connect("localhost", "aditi","Srinagar@123", "blog_web") or die ("db not connected");
+?>
---
+++ login.php
@@ -0,0 +1,61 @@
+<?php 
+include 'config.php';
+include 'header.php';
+session_start();
+if(isset($_SESSION ['user_data'])){
+    header("location:http://localhost/blog/admin/index.php");
+ }
+?>
+
+<div class = "container">
+    <div class = "row"> 
+        <div class="col-xl-5 col-md-4 m-auto p-5 mt-5 bg-info ">
+            <p class="text-center">Blog! Login your account.</p>
+            <form action="" method= "POST">
+            <div class="mb-3">
+                <input type = "email" name="email" placeholder="Email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <input type = "password" name="password" placeholder="Password" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <input type = "submit" name="login_btn" class="btn btn-primary" value ="login">
+            </div>
+
+            <?php
+            if(isset($_SESSION['error'])){
+               $error = $_SESSION['error'];
+               echo "<p class ='bg-danger p-2 text-white'>" .$error. "</p>";
+               unset($_SESSION['error']);
+            }
+            ?>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php';
+if(isset($_POST['login_btn'])){
+    $email=mysqli_real_escape_string($config, $_POST['email']);
+    $pass=mysqli_real_escape_string($config, sha1($_POST['password']));
+
+    // FETCH THE DATA
+    $sql = "SELECT *FROM user WHERE email='{$email}' AND password='{$pass}' ";
+    $query=mysqli_query($config, $sql);
+    $data = mysqli_num_rows($query);
+    if($data){
+        $result=mysqli_fetch_assoc($query);
+        $user_data= array($result['user_id'],$result['user_name'],$result['roll']);
+        $_SESSION['user_data']= $user_data;
+
+        //redirect to page
+        header("location:admin/index.php");
+    }
+    else{
+        
+        $_SESSION['error']="invalid email/password";
+        header("location:login.php");
+}
+}
+
+
